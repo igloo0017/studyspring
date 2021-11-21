6d2d47d8989af696ba93e3b6ec3ac7820c073eca
igloo0017 <rlgusqls@naver.com>
2021-11-21 16:36:58 +0900

    study

---
+++ servlet/src/test/java/hello/servlet/domain/Member/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package hello.servlet.domain.Member;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach () {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+        //when
+        Member savedMember = memberRepository.save(member);
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        Assertions.assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void findAll(){
+
+        //given
+
+        //when
+
+        //then
+    }
+}
